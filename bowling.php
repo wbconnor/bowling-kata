26dca78643fc38412d95a2843c84a35217059dec
drmyersii <davidrmyersii@gmail.com>
2016-02-27 16:08:36 -0500

    Adding tests in bowling script.

--- bowling.php
+++ bowling.php
@@ -1,18 +1,40 @@
 <?php
 
+require_once (__DIR__ . '/Game.php');
 require_once (__DIR__ . '/Frame.php');
 require_once (__DIR__ . '/EndFrame.php');
 
-$frame = new EndFrame();
+// $frame = new EndFrame();
+//
+// $frame->roll(3);
+// $frame->roll(7);
+// $frame->roll(10);
 
-$frame->roll(3);
-$frame->roll(7);
-$frame->roll(10);
+$game = new Game();
 
-var_dump ($frame->isFinished());
+$game->roll(1);
+$game->roll(3);
+
+$game->roll(10);
+
+$game->roll(9);
+$game->roll(0);
+
+$game->roll(10);
+
+$game->roll(10);
+
+$game->roll(10);
+
+$game->roll(10);
+
+$game->roll(10);
+
+$game->roll(10);
+
+$game->roll(9);
+$game->roll(1);
+$game->roll(1);
+
+var_dump ($game->isFinished());
 echo PHP_EOL;
-
-
-// $game = new Game();
-
-// $game->run();
