1742d2a2de21dd984acb03ae841ff4c21657e2c9
drmyersii <davidrmyersii@gmail.com>
2016-02-27 12:02:30 -0500

    Creating Game class. Adding two public methods for interacting with the game.

---
+++ Game.php
@@ -0,0 +1,25 @@
+<?php
+
+class Game
+{
+    /**
+     * The ball rolls and knocks over the given number of pins.
+     *
+     * @param $pins int
+     * @return void
+     */
+    public function roll($pins)
+    {
+
+    }
+
+    /**
+     * Return the total score for the game.
+     *
+     * @return int
+     */
+    public function score()
+    {
+
+    }
+}
