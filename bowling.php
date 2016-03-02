4a8985928a8f6b89dc75e02f9c7bc9460e8a02c0
drmyersii <davidrmyersii@gmail.com>
2016-03-02 15:41:38 -0500

    Adding documentation.

--- bowling.php
+++ bowling.php
@@ -4,5 +4,6 @@ require_once (__DIR__ . '/Game.php');
 require_once (__DIR__ . '/Frame.php');
 require_once (__DIR__ . '/FinalFrame.php');
 
+// create a new game and run it
 $game = new Game();
 $game->run();
