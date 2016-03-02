fb307664b39721ec3545aaf2068ebfdf09eb53b1
drmyersii <davidrmyersii@gmail.com>
2016-03-02 14:42:10 -0500

    Adding input(), inputRaw(), output(), and error() methods to Cli class.

--- Cli.php
+++ Cli.php
@@ -2,5 +2,38 @@
 
 class Cli
 {
-    
+    public function __construct()
+    {
+        // initialize
+    }
+
+    public function input()
+    {
+        return trim($this->inputRaw());
+    }
+
+    public function inputRaw()
+    {
+        return fgets(STDIN);
+    }
+
+    public function output($output = '', $newline = true)
+    {
+        if ($newline)
+        {
+            $output = PHP_EOL . $output;
+        }
+
+        fwrite(STDOUT, $output);
+    }
+
+    public function error($error = '', $newline = true)
+    {
+        if ($newline)
+        {
+            $error = PHP_EOL . $error;
+        }
+
+        fwrite(STDERR, $error);
+    }
 }
