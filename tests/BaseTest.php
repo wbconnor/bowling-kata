969c8f26f79f93f5d5ae88caca4a342ae82d03e3
drmyersii <davidrmyersii@gmail.com>
2016-03-05 13:33:46 -0500

    Adding BaseTest with a protected method to invoke private/protected methods for a given object.

---
+++ tests/BaseTest.php
@@ -0,0 +1,22 @@
+<?php
+
+class BaseTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Call a private/protected method of the given object.
+     *
+     * @param $object object
+     * @param $method string
+     * @param $params array
+     * @return mixed
+     */
+    protected function invokeMethod($object, string $method, array $params = [])
+    {
+        $reflection        = new \ReflectionClass(get_class($object));
+        $reflection_method = $reflection->getMethod($method);
+
+        $reflection_method->setAccessible(true);
+
+        return $reflection_method->invokeArgs($object, $params);
+    }
+}
