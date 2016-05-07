32cc319ce270dfe21ab3f88666de5d5189ca3043
drmyersii <davidrmyersii@gmail.com>
2016-05-07 14:48:05 -0400

    Removing unnecessary dependencies for the bowling script.

--- bowling.php
+++ bowling.php
@@ -1,8 +1,6 @@
 <?php
 
 require_once (__DIR__ . '/Game.php');
-require_once (__DIR__ . '/Frame.php');
-require_once (__DIR__ . '/FinalFrame.php');
 
 // create a new game and run it
 $game = new Game();
